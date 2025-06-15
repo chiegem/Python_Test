82050e74756ab2e6cc6b81d69a78875a892d05bc
chiegem <c.nakano.devlog+gem@gmail>
2025-06-15 15:41:45 +0900

    [add] example.py

---
+++ example.py
@@ -0,0 +1,22 @@
+print('hello world')
+
+def greet():
+    print('こんにちは')
+
+greet()
+
+def print_name(name):
+    print(f'私の名前は{name}です')
+
+print_name('ちえ@56期')
+
+def get_greet():
+    return 'おはようございます'
+
+print(get_greet())
+
+def add(a, b):
+    return a + b
+
+print(add(1000, 10))
+
